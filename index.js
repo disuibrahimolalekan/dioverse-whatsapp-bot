3302690ff6d50fbc31e9ad8a144e08dac497705d
disuibrahimolalekan <disuibrahimolalekan@gmail.com>
2026-03-07 17:39:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.static('public'));
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const MONGODB_URI = process.env.MONGODB_URI;
 const SHEETS_URL = 'https://script.google.com/macros/s/AKfycbzWBvCN5yTlW0VlAvnskunnmoargP7iLS4fnDVgyKt2trIPN-0P8dAtZcUZJlDNvftxMg/exec';
+
 let db;
 const pendingLeads = {};
 
